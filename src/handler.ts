e769e81a65d97b061824abb28d827286119c702d
adamdubicki <adam@freshworks.io>
2019-04-16 12:08:31 -0700

    starting to test

--- src/handler.ts
+++ src/handler.ts
@@ -4,6 +4,9 @@ import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
 import { getBuildRecordFromS3, saveBuildRecordToS3 } from './helper/s3Helper';
 import { lambdaResponse } from './helper/handlerHelper';
 import { sendMessage, formatBuildMessage } from './helper/slackHelper';
+import SheetsAPI from 'sheets-api';
+
+const sheets = new SheetsAPI();
 
 export const updateBuildRecord: Handler = async (event: APIGatewayEvent, context: Context, cb: Callback) => {
   try {
@@ -33,19 +36,34 @@ export const getCurrentBuildRecord: Handler = async (event: APIGatewayEvent, con
 
 export const startPerformanceTest: Handler = async (event: APIGatewayEvent, context: Context, cb: Callback) => {
   try {
-    const { data } = await axios({
-      method: 'post',
-      url: 'https://yellowlab.tools/api/runs',
-      data: {
-        url: 'https://nodeca.stage.aplaceformom.com/en',
-        device: 'desktop',
-        screenshot: true,
-        waitForResponse: false,
-      },
-      headers: {
-        contentType: 'application/json'
+    let payload = {
+      spreadsheetId: '1dlFsm2Mdi0-bLPEjfcfDwZc7NRgJScHg7unJl4NxKoo',
+      range: "Orders!A1:D1",
+      valueInputOption: 'USER_ENTERED',
+      resource : {
+        majorDimension: "ROWS",
+        values: [
+          ["Door", "$15", "2", "3/15/2017"],
+          ["Engine", "$100", "1", "3/20/2016"]
+        ]
       }
-    });
+    }
+    sheets
+      .authorize()
+      .then(auth => sheets.values('append', auth, payload))
+    // const { data } = await axios({
+    //   method: 'post',
+    //   url: 'https://yellowlab.tools/api/runs',
+    //   data: {
+    //     url: 'https://nodeca.stage.aplaceformom.com/en',
+    //     device: 'desktop',
+    //     screenshot: true,
+    //     waitForResponse: false,
+    //   },
+    //   headers: {
+    //     contentType: 'application/json'
+    //   }
+    // });
     return lambdaResponse(200, {})
   } catch(e) {
     return lambdaResponse(400, { message: e });
